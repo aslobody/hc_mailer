a2c283712558d0826a8162f021a7361b51197604
Anatoliy Slobodyanyuk <anatoliy.slobodyanyuk@canada.ca>
2020-05-11 10:20:28 -0400

    Add language to captcha

--- captcha.js
+++ captcha.js
@@ -1,16 +1,28 @@
 $(function() {
-  var text = '<div class="form-group">' +
+  var textEn = '<div class="form-group">' +
     '<label for="captchaValidation" class="required" id="captchaError"><span class="field-name">To prove you are a person please enter today\'s date</span><span class="datepicker-format"> (YYYY-MM-DD)</span> <strong class="required">(required)</strong></label>' +
     '<input class="form-control" id="captchaValidation" name="captchaValidation" type="date" data-rule-dateISO="true" required />' +
     '</div>';
-  $("#captcha").append(text);
+  var textFr = '<div class="form-group">' +
+    '<label for="captchaValidation" class="required" id="captchaError"><span class="field-name">Pour prouver que vous êtes une personne, veuillez entrer la date d\'aujourd\'hui</span><span class="datepicker-format"> (YYYY-MM-DD)</span> <strong class="required">(obligatoire)</strong></label>' +
+    '<input class="form-control" id="captchaValidation" name="captchaValidation" type="date" data-rule-dateISO="true" required />' +
+    '</div>';
+  var appendErrorEn = '<strong class="error"><span class="label label-danger"><span class="prefix">Error: </span>Please enter today\'s date</span></strong>';
+  var appendErrorFr = '<strong class="error"><span class="label label-danger"><span class="prefix">Erreur : </span>Veuillez entrer la date du jour</span></strong>';
+  var lang = $("html").attr("lang");
+  if (lang === 'en') {
+    $("#captcha").append(textEn);
+  } else if (lang === 'fr') {
+    $("#captcha").append(textFr);
+  }
   // validate today's date
   $("#captchaValidation").on("blur", function() {
     var str = $(this).val();
     var today = new Date().toISOString().slice(0, 10);
-    //alert(str);
-    if (str !== today) {
-      $("#captchaError").append('<strong class="error"><span class="label label-danger"><span class="prefix">Error: </span>Please enter today\'s date</span></strong>');
+    if (str !== today && lang === 'en') {
+      $("#captchaError").append(appendErrorEn);
+    } else if (str !== today && lang === 'fr') {
+      $("#captchaError").append(appendErrorFr);
     } else {
       $("#captchaError strong.error").remove();
     }
